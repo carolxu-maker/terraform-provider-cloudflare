9094a00df48b4fbef0bb823bfd4d10efddc5230a
Carol Xu <cxu@cloudflare.com>
2025-09-18 13:54:23 -0500

    Add basic update/read tests for CORS rules on bucket

---
+++ internal/services/r2_bucket_cors/testdata/r2bucketcorsrequired_only.tf
@@ -0,0 +1,10 @@
+resource "cloudflare_r2_bucket" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+}
+
+resource "cloudflare_r2_bucket_cors" "%[1]s" {
+  account_id  = "%[2]s"
+  bucket_name = cloudflare_r2_bucket.%[1]s.name
+  # No rules specified - testing minimal configuration
+}
